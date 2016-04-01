46558ed8c591354a2014201c188b72341861f544
Kyle Harvey <kyle@kyleharvey.com>
2016-03-31 22:38:00 -0700

    simplified checkMove(), added usernotes
    eventListeners:
        keydown for toggling user notes with 'n' (preventDefault for n),
        keypress for calling functions checkMove/userNotes (preventDefault for non digit),
        keyup for delete.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function Sudoku(puzzle) {
     var sudoku = document.getElementById('sudoku');
     var spaces = document.querySelectorAll("#sudoku table input");
+    var toggleNotes = document.getElementById('tnotes');
     fillPuzzle();
 
     //
@@ -12,6 +13,8 @@ function Sudoku(puzzle) {
                 //add data-(x/y) to spaces for specific access
                 spaces[row*9+col].dataset.x = row;
                 spaces[row*9+col].dataset.y = col;
+                //add span for notes
+                spaces[row*9+col].insertAdjacentHTML('afterend', '<span class="notes"></span>');
                 if(puzzle[row][col] !== 0) {
                     spaces[row*9+col].value = puzzle[row][col];
                     spaces[row*9+col].readOnly = true;
@@ -63,20 +66,27 @@ function Sudoku(puzzle) {
 
     //
     //change to input: make sure input is num and check for winner.
-    function checkMove(e) {
-        var x,y,v;
-        x = parseInt(e.target.dataset.x, 10);
-        y = parseInt(e.target.dataset.y, 10);
-        v = e.target.value;
-        if(v.match(/[1-9]/)) { //if value is 1-9 
-            e.target.select(); //keep selected for ux
-            e.target.classList.add('num');
+    function checkMove(evt) {
+        evt.target.classList.add('num');
+        setTimeout(function() {
+            evt.target.select(); //keep selected for ux
             checkWin();
-        } else { //value deleted or is not valid
-            e.target.value = ''; 
-            e.target.classList.remove('num');
-            e.target.classList.remove('wrong');
+        }, 0);
+    }
+
+    //
+    //add user note
+    function userNotes(evt) {
+        var el,v,notes;
+        el = evt.target.nextSibling;
+        v = String.fromCharCode(evt.keyCode);
+        notes = el.textContent.split('');
+        if(notes.indexOf(v) >= 0) {
+            notes.splice(notes.indexOf(v), 1);
+        } else {
+            notes.push(v);
         }
+        el.innerHTML = notes.sort().join('');
     }
 
     //Navigate puzzle with arrow keys
@@ -107,7 +117,29 @@ function Sudoku(puzzle) {
     };
 
     //event listeners
-    sudoku.addEventListener('input', checkMove, false); //any change to input
+    document.addEventListener('keydown', function(evt) {
+        if(evt.keyCode == 78) { //n
+            toggleNotes.checked = !toggleNotes.checked;
+            evt.preventDefault();
+        }
+    });
+    sudoku.addEventListener('keypress', function(evt) {
+        if (evt.keyCode > 48 && evt.keyCode < 58) {
+            if(toggleNotes.checked) {
+                evt.preventDefault();
+                userNotes(evt);
+            } else {
+                checkMove(evt);
+            }
+        } else {
+            evt.preventDefault();
+        }
+    }, false); //any change to input
+    sudoku.addEventListener('keyup', function(evt) {
+        if (evt.keyCode == 8) {
+            evt.target.classList.remove('num');
+        }
+    }, false);
     sudoku.addEventListener('focus', function(evt) { 
         if (evt.target.matches('input')) {
             evt.target.select(); 
@@ -116,13 +148,22 @@ function Sudoku(puzzle) {
     sudoku.addEventListener('keydown', goToSpace, false);
 };
 Sudoku(
-   [[5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]]
+     [[5,3,0,0,7,8,9,1,2],
+      [6,7,2,1,9,5,3,4,8],
+      [1,9,8,3,4,2,5,6,7],
+      [8,5,9,7,6,1,4,2,3],
+      [4,2,6,8,5,3,7,9,1],
+      [7,1,3,9,2,4,8,5,6],
+      [9,6,1,5,3,7,2,8,4],
+      [2,8,7,4,1,9,6,3,5],
+      [3,4,5,2,8,6,1,7,9]]
 );
+   //[[5,3,0,0,7,0,0,0,0],
+    //[6,0,0,1,9,5,0,0,0],
+    //[0,9,8,0,0,0,0,6,0],
+    //[8,0,0,0,6,0,0,0,3],
+    //[4,0,0,8,0,3,0,0,1],
+    //[7,0,0,0,2,0,0,0,6],
+    //[0,6,0,0,0,0,2,8,0],
+    //[0,0,0,4,1,9,0,0,5],
+    //[0,0,0,0,8,0,0,7,9]]
